41e9a56cf4c7dac36c0a3e9b02776b01acde4f5d
Olamide Olaleye <olamide@intheclouds365.com>
2022-09-27 12:54:08 +0100

    added ssh config

--- ssh/sshkey.ps1
+++ ssh/sshkey.ps1
@@ -1,5 +1,5 @@
 # create ssh key and upload to azure key vault using azure cli
 # Genrate ssh key
 #
-ssh-keygen -m PEM -t rsa -b 4096
-az keyvault secret set --name sshkeys --vault-name labuksrgkeystore --file "C:\Users\Olamide\.ssh\id_rsa.pub" --encoding ascii
+ssh-keygen -m PEM -t rsa -b 4096 -C "zadmin@azubuntu2204"
+az keyvault secret set --name SSHPublicKey --vault-name azuksengrrgkv --file "C:\Users\olamide\.ssh\id_rsa.pub" --encoding ascii
